7e53e431910aa44662bfca2babf231eee90e0d90
Natalia Troyanovskaya <nataliatroyanovskaya@MacBook-Pro-Natalia-2.local>
2020-11-11 12:08:43 +0300

    v shkolly

--- inits.c
+++ inits.c
@@ -6,10 +6,52 @@
 /*   By: scopycat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/08 22:45:37 by scopycat          #+#    #+#             */
-/*   Updated: 2020/11/08 22:46:42 by scopycat         ###   ########.fr       */
+/*   Updated: 2020/11/10 21:19:55 by scopycat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// напишу, как буду более четко представлять структуру
+void	init_com(t_command *com)
+{
+	int i;
+	
+	i = -1;
+	init_comd(com);
+	init_arg(com);
+	com->env_var = NULL;
+	com->no_arg = 1; // в целом тут можно не инициализировать, потому что дальше инициализируетя (эти три счетчика)
+	com->no_command = 1;
+	com->no_var = 1;
+	com->pipe_count = 0;
+	while (++i < 100) // проверить, что инкремент так как надо работает
+		com->process[i] = 0;
+}
+
+void	init_comd(t_command *com)
+{
+	com->comd = (t_comd*)malloc(sizeof(t_comd));
+	com->comd->cmnd = NULL;
+	init_flag(com);
+	com->comd->no_command = 1;
+	com->comd->env_var = NULL;
+	com->comd->next = NULL;
+}
+
+void	init_flag(t_command *com)
+{
+	com->comd->flag = (t_flag*)malloc(sizeof(t_flag));
+	com->comd->flag->flag = NULL;
+	com->comd->flag->no_flag = 1;
+	com->comd->flag->next = NULL;
+}
+
+void	init_arg(t_command *com)
+{
+	com->arg = (t_arg*)malloc(sizeof(t_arg));
+	com->arg->arg = NULL;
+	com->arg->path = NULL;
+	com->arg->wildcard = 0;
+	com->arg->no_arg = 1;
+	com->arg->next = NULL;
+}
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: scopycat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 14:50:14 by scopycat          #+#    #+#             */
-/*   Updated: 2020/11/08 23:00:43 by scopycat         ###   ########.fr       */
+/*   Updated: 2020/11/10 23:04:27 by scopycat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,45 +18,55 @@
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
-# include "./headers/get_next_line.h"
+# include "get_next_line.h"
 
-typedef struct	s_flag
+typedef struct		s_flag
 {
-	char		*flag;
-	char		*next;
-}				t_flag;
+	struct s_flar	*next;
+	char			*flag;
+	size_t			no_flag; // изначально 1, если флаги не найдены, то обнуляется
+}					t_flag;
 
-typedef struct	s_comd
+typedef struct		s_comd
 {
-	char		*cmnd;
-	t_flag		*flag;
-	char		*env_var; // переменная для команды
-	char		*next;
-}				t_comd;
+	struct s_comd	*next;
+	char			*cmnd;
+	t_flag			*flag;
+	char			*env_var; // переменная для команды
+	size_t			no_command; // изначально 1, если команды не найдены, то обнуляется
+}					t_comd;
 
 typedef struct	s_arg
 {
-	char		*arg;
-	size_t		wildcard; // для бонуса если 0, но звездочки нет, если 1, то есть и можно вспомнить matchtomatch
-	char		*next;
-}				t_arg;
+	struct s_arg	*next;
+	char			*arg;
+	char			*path;
+	size_t			wildcard; // для бонуса если 0, но звездочки нет, если 1, то есть и можно вспомнить matchtomatch
+	size_t			no_arg; // изначально  1, если аргументы не найдены, то обнуляется
+}					t_arg;
 
-typedef struct	s_command
+typedef struct		s_command
 {
-	t_comd		*comd;
-	t_arg		*arg;
-	char		*env_var; // переменная, которая просто выводится (или исполняется)
-	size_t		no_command; //обнуляется, если нет команды
-	size_t		no_arg; // обнуляется, если нет аргументов
-	size_t		no_var; // обнуляется, если нет переменной окружения
-	size_t		pipe_count;
-	size_t		process[1028]; // не помню для чего сделала, но кажется нужно
-}				t_command;
+	t_comd			*comd;
+	t_arg			*arg;
+	char			*env_var; // переменная, которая просто выводится (или исполняется)
+	size_t			no_command; //обнуляется, если нет команды. изначально количество pipe + 1
+	size_t			no_arg; // обнуляется, если нет аргументов. изначально количество pipe + 1
+	size_t			no_var; // обнуляется, если нет переменной окружения. изначально количество pipe + 1
+	size_t			pipe_count;
+	size_t			process[100]; // сюда пишутся переменные, которые возвращает fork
+}					t_command;
 
-void			parser(char *line, t_command *com);
-void			pars_pipes(char *line, t_command *com);
-void			work_comman(t_command *com);
-void			free_all(t_command *com);
-void			init(t_command *com);
+int					main(void);
+void				parser(char *line, t_command *com);
+void				pars_pipes(char *line, t_command *com);
+void				pars_commands(char *line, t_command *com);
+void				work_comman(t_command *com);
+void				free_all(t_command *com);
+void				init_com(t_command *com);
+void				init_comd(t_command *com);
+void				init_flag(t_command *com);
+void				init_arg(t_command *com);
+char				**ft_split(char const *s, char c);
 
 #endif
